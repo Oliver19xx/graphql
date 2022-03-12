9d0c386985ba42f99ec1cb3654e2c8935d43147c
Oliver Werner <oliver.werner@hs-osnabrueck.de>
2022-03-12 15:55:20 +0100

    BookList hinzugefügt

---
+++ client/src/components/BookList.js
@@ -0,0 +1,13 @@
+
+
+function BookList() {
+    return (
+        <div>
+            <ul id="book-list">
+                <li>Book name</li>
+            </ul>
+        </div>
+    );
+}
+
+export default BookList;
