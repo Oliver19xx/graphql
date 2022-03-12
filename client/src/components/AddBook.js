4c5aa0905b065bcd959e30b1cfbbcf0e47e6a84f
Oliver Werner <oliver.werner@hs-osnabrueck.de>
2022-03-12 19:36:56 +0100

    External Query File

--- client/src/components/AddBook.js
+++ client/src/components/AddBook.js
@@ -1,13 +1,5 @@
-import { gql, graphql, useQuery } from "@apollo/client";
-
-const getAuthorsQuery = gql`
-{
-    authors{
-        name,
-        id
-    }
-}
-`
+import { useQuery } from "@apollo/client";
+import { getAuthorsQuery } from "../queries/queries";
 
 function AddBook() {
     const { loading, error, data } = useQuery(getAuthorsQuery);
