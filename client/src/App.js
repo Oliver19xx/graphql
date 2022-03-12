e73fea77ad6e912bd3e6e7225e96ec0d686fcca3
Oliver Werner <oliver.werner@hs-osnabrueck.de>
2022-03-12 16:06:23 +0100

    Apollo Client Setup

--- client/src/App.js
+++ client/src/App.js
@@ -1,12 +1,24 @@
 import BookList from "./components/BookList";
+import {
+  ApolloClient,
+  InMemoryCache,
+  ApolloProvider,
+  useQuery,
+  gql
+} from "@apollo/client";
 
+const client = new ApolloClient({
+  uri: 'http://localhost:4000/graphql'
+})
 
 function App() {
   return (
+    <ApolloProvider client={client}>
     <div id="main">
       <h1>Reading List</h1>
       <BookList/>
     </div>
+    </ApolloProvider>
   );
 }
 
