9fdd647fcbeda1a635fff87deb88101d0ede1738
Oliver Werner <oliver.werner@hs-osnabrueck.de>
2022-03-12 23:03:18 +0100

    Create Book and refetch list

--- client/src/components/AddBook.js
+++ client/src/components/AddBook.js
@@ -1,35 +1,47 @@
-import { useQuery } from "@apollo/client";
+import { useMutation, useQuery } from "@apollo/client";
 import { useState } from "react";
-import { getAuthorsQuery } from "../queries/queries";
+import { addBookMutation, getAuthorsQuery, getBooksQuery } from "../queries/queries";
 
 function AddBook() {
+    let inputName, inputGenre, inputAuthorID;
     const [name, setName] = useState('');
     const [genre, setGenre] = useState('');
     const [authorId, setAuthorId] = useState('');
 
     const { loading, error, data } = useQuery(getAuthorsQuery);
-
+    const [addBook] = useMutation(addBookMutation);
     if (error) return <p>Error :(</p>;
 
     const handleSubmit = (e) => {
         e.preventDefault();
-        console.log(name, genre, authorId);
+        addBook({
+            variables: {
+                name: inputName.value,
+                genre: inputGenre.value,
+                authorId: inputAuthorID.value
+            }, refetchQueries: [
+                { query: getBooksQuery }
+            ]
+        });
+        inputName.value = '';
+        inputGenre.value = '';
+        inputAuthorID.value = '';
     }
 
     return (
         <form id="add-book" onSubmit={handleSubmit}>
             <div className="field">
                 <label>Book name:</label>
-                <input type="text" value={name} onChange={(e) => setName(e.target.value)} />
+                <input type="text" ref={node => { inputName = node; }} value={name} onChange={(e) => setName(e.target.value)} />
             </div>
             <div className="field">
                 <label>Genre:</label>
-                <input type="text" value={genre} onChange={(e) => setGenre(e.target.value)} />
+                <input type="text" ref={node => { inputGenre = node; }} value={genre} onChange={(e) => setGenre(e.target.value)} />
             </div>
             <div className="field">
                 <label>Author:</label>
-                <select value={authorId} onChange={(e) => setAuthorId(e.target.value)}>
-                    <option>{loading ? 'Authors loading' : 'Select author'}</option>
+                <select value={authorId} ref={node => { inputAuthorID = node; }} onChange={(e) => setAuthorId(e.target.value)}>
+                    <option value={null}>{loading ? 'Authors loading' : 'Select author'}</option>
                     {data?.authors.map(author => <option key={author.id} value={author.id}>{author.name}</option>)}
                 </select>
             </div>
